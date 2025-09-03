6d9288471d853b89536ef5039dba832c0fb6f87f
madhav40 <pujarimadhav023@gmail.com>
2025-09-03 17:18:25 +0530

    first commit

---
+++ Week_01/Basic.py
@@ -0,0 +1,13 @@
+# age = 22
+# Name = "Madhav Pujari"
+# print( Name , "is a good boy" )
+
+
+# First = float(input("Enter the value 1st "))
+# Second  = float(input("Enter the value of 2nd "))
+# Sum = First + Second 
+# print("Sum: " + str(Sum))
+# print(type(Sum))
+
+Madhav = 'is a gamer'
+print(Madhav.find('a'))
